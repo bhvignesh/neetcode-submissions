8b54d27fef5946639c0bb27f4e6c3d43d288542c
Vignesh <bhvignesh@gmail.com>
2026-05-13 16:31:48 +0530

    Add: remove-element - submission-7

---
+++ Data Structures & Algorithms/remove-element/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        val_index = 0
+        non_val_index = len(nums) -1
+        while val_index <= non_val_index:
+            if (nums[val_index] == val) and (nums[non_val_index] != val):
+                nums[val_index], nums[non_val_index] = nums[non_val_index], nums[val_index]
+            if (nums[val_index] != val):
+                val_index += 1
+            if (nums[non_val_index] == val):
+                non_val_index -= 1
+        return val_index
